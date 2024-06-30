eff74a9d95a582ea13e2c93bddf20ae9b4e47c9f
fhina12 <masythafhina@gmail.com>
2024-06-30 04:35:55 -0700

    Add files via upload

---
+++ Day16/menggabungkannilaistring.py
@@ -0,0 +1,7 @@
+# Program untuk menggabungkan dua string
+kata1 = input("Masukkan kata pertama: ")
+kata2 = input("Masukkan kata kedua: ")
+
+kata_gabungan = kata1 + " " + kata2
+
+print("Kata yang digabungkan:", kata_gabungan)
