a36426948ccde0bd87ed931c564042ebb068e538
fhina12 <masythafhina@gmail.com>
2024-07-01 08:26:46 -0700

    Add files via upload

---
+++ Day17/memeriksathnkabisat.py
@@ -0,0 +1,7 @@
+# Program Memeriksa Tahun Kabisat
+tahun = int(input("Masukkan tahun: "))
+
+if (tahun % 4 == 0) and (tahun % 100 != 0) or (tahun % 400 == 0):
+    print(tahun, "adalah tahun kabisat")
+else:
+    print(tahun, "bukan tahun kabisat")
