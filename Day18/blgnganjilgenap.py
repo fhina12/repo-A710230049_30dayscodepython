66edc68c85fcf3afecf2ce6ddc9251054345eb76
fhina12 <masythafhina@gmail.com>
2024-07-01 23:17:05 -0700

    Add files via upload

---
+++ Day18/blgnganjilgenap.py
@@ -0,0 +1,7 @@
+# Program untuk memeriksa bilangan ganjil/genap
+bilangan = int(input("Masukkan bilangan: "))
+
+if bilangan % 2 == 0:
+    print(bilangan, "adalah bilangan genap")
+else:
+    print(bilangan, "adalah bilangan ganjil")
