909328f7a72467fd7ae7ef4194f5024dd1f8fe83
fhina12 <masythafhina@gmail.com>
2024-07-03 21:30:31 -0700

    Add files via upload

---
+++ Day20/mengubahsuhucelciuskefarenheit.py
@@ -0,0 +1,6 @@
+# Program untuk mengubah suhu Celcius ke Fahrenheit
+celcius = float(input("Masukkan suhu Celcius: "))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(celcius, "derajat Celcius sama dengan", fahrenheit, "derajat Fahrenheit")
